3ee3497f204050de6464fa3b091fe477408a6b40
Sander Soikka <sander.soikka@gmail.com>
2022-05-05 23:56:54 +0300

    Control Flow

---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem, kui 18, siis programm kuvab konsoolis
+            //"Oled liiga noor, et juhilube taotelda"
+            //Kui kasutaja on vanem, kui 18 siis programm kuvab konsoolis
+            //"Oled piisavalt vana, et juhilube taotelda"
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd saad juhilube taotelda!"
+
+            Console.WriteLine("Palun sisesta oma sünniaasta:");
+
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube taotelda.");
+            }
+            else if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube taotelda.");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd saad juhilube taotelda!");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ Userdate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Userdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust; 
+            //programm kuvab kasutaja vanust konsoolis; 
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+        }
+    }
+}
